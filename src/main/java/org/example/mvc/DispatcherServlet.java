f47ac3e06d363e5e6ca6921b7aa1c11967b2b0f1
webdevyoo <yjk7454@naver.com>
2023-11-13 02:43:51 +0900

    feat: DispatcherServlet 클래스 - 기본 요청 url 컨트롤러 클래스 매핑 초기화, 요청url 정보 위임으로 컨트롤러 반환 및 뷰리졸버 반환, forward처리

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,13 +1,17 @@
 package org.example.mvc;
 
+import org.example.mvc.controller.Controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/")
@@ -15,8 +19,33 @@ public class DispatcherServlet extends HttpServlet {
 
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
+    private RequestMappingHandlerMapping requestMappingHandlerMapping;
+
+    /**
+     * 톰캣이 http Servlet을 싱글톤 객체 하나로 만든다.
+     * 이때, init 메소드가 호출된다.
+     * 해당 시점에 RequestMappingHandlerMapping의 초기화 메소드를 통해 map을 초기화한다.
+     * @throws ServletException
+     */
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
-        log.info("DispatcherServlet#service");
+    public void init() throws ServletException {
+        requestMappingHandlerMapping = new RequestMappingHandlerMapping();
+        requestMappingHandlerMapping.init();
     }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.info("DispatcherServlet#service");
+
+        Controller handler = requestMappingHandlerMapping.findHandler(request.getRequestURI());// 현재 요청 uri 정보를 handler메소드에 위임
+        try {
+            String viewName = handler.handleRequest(request, response);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
+            requestDispatcher.forward(request, response);
+        } catch (Exception e) {
+            log.error("exception occurred: [{}]", e.getMessage(), e);
+            throw new ServletException(e);
+        }
+    }
+
 }
---
+++ webapps/home.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!doctype html>
+<html lang="kr">
+<head>
+    <meta charset="UTF-8">
+    <title>home</title>
+</head>
+<body>
+HOME 페이지
+</body>
+</html>
