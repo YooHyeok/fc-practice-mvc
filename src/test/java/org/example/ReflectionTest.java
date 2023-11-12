d65b796099287fe3a63b4b8a652d9ad14d5b1a9d
webdevyoo <yjk7454@naver.com>
2023-11-13 00:07:42 +0900

    refector: ReflectionTest controllerScan Reflections 로직 메소드 추출 리팩토링 controllerScanRefector() 테스트 메소드 추가

--- src/test/java/org/example/ReflectionTest.java
+++ src/test/java/org/example/ReflectionTest.java
@@ -9,7 +9,9 @@ import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.annotation.Annotation;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -31,5 +33,28 @@ public class ReflectionTest {
         beans.addAll(reflections.getTypesAnnotatedWith(Controller.class)); // Controller 클래스 어노테이션이 선언된 클래스들을 찾은뒤 HashSet에 모두 저장
         beans.addAll(reflections.getTypesAnnotatedWith(Service.class));
         logger.debug("beans: [{}]", beans);
+    }
+
+
+    @Test
+    void controllerScanRefector() {
+        Set<Class<?>> beans = getTypesAnnotatedWith(List.of(Controller.class, Service.class));
+        logger.debug("beans: [{}]", beans);
+    }
+
+    /**
+     *
+     * @param annotations
+     * Class<? extends Annotaion > : Annotation 타입의 클래스를 담는 리스트를 뜻한다. <br/>
+     * 각 항목은 어노테이션을 나타내는 클래스의 Class객체이다.
+     * ? extends Annotation은 해당 클래스가 어노테이션 타입이거나 어노테이션 타입의 서브타입임을 의미한다.
+     * @return
+     */
+    private static Set<Class<?>> getTypesAnnotatedWith(List<Class<? extends Annotation>> annotations) {
+        Reflections reflections = new Reflections("org.example"); //패키지 하위의 모든 클래스 대상으로 리플렉션을 사용한다.
+        Set<Class<?>> beans = new HashSet<>();
+        annotations.forEach(annotation -> beans.addAll(reflections.getTypesAnnotatedWith(annotation)));
+
+        return beans;
     }
 }
