524fb40acac055f8a4e4a86ce03b2229729d217a
webdevyoo <yjk7454@naver.com>
2023-11-14 00:29:54 +0900

    feat: UserListController - handleRequest() - 요청 객체에 데이터 전달 로직 추가

--- src/main/java/org/example/mvc/controller/UserListController.java
+++ src/main/java/org/example/mvc/controller/UserListController.java
@@ -2,10 +2,12 @@ package org.example.mvc.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 public class UserListController implements Controller {
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.setAttribute("users", List.of()); //users라는 이름으로 List데이터 요청 객체에 담아 view에 전달
         return "/user/list.jsp"; // webapps/user/ 디렉토리 하위경로
     }
 }
