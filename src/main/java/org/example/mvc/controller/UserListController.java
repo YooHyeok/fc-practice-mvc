3a6300691f66779b88dc96b4c5f16b971b60f4fb
webdevyoo <yjk7454@naver.com>
2023-11-15 00:54:56 +0900

    feat: UserRepository - findAll() 메소드 추가 및 구현 / UserListController의 setAttribute에 적용

--- src/main/java/org/example/mvc/controller/UserListController.java
+++ src/main/java/org/example/mvc/controller/UserListController.java
@@ -1,5 +1,7 @@
 package org.example.mvc.controller;
 
+import org.example.mvc.repository.UserRepository;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -7,8 +9,9 @@ import java.util.List;
 public class UserListController implements Controller {
     @Override
     public String handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        request.setAttribute("users", List.of()); //users라는 이름으로 List데이터 요청 객체에 담아 view에 전달
+//        request.setAttribute("users", List.of()); //users라는 이름으로 List데이터 요청 객체에 담아 view에 전달
 //        return "/user/list.jsp"; // webapps/user/ 디렉토리 하위경로
+        request.setAttribute("users", UserRepository.findAll()); //users라는 이름으로 List데이터 요청 객체에 담아 view에 전달
         return "/user/list"; // JspViewResolver에 의해 .jsp가 자동으로 붙는다
     }
 }
