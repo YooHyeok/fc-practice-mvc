94e96e510d45c631434ca4159ee372d9ecbeb812
webdevyoo <yjk7454@naver.com>
2023-11-15 00:42:17 +0900

    fix: DispatcherServlet에 ViewResolver 기능 추가 및 구현

--- src/main/java/org/example/mvc/DispatcherServlet.java
+++ src/main/java/org/example/mvc/DispatcherServlet.java
@@ -1,6 +1,9 @@
 package org.example.mvc;
 
 import org.example.mvc.controller.Controller;
+import org.example.mvc.view.JspViewResolver;
+import org.example.mvc.view.View;
+import org.example.mvc.view.ViewResolver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,6 +15,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 @WebServlet("/")
 public class DispatcherServlet extends HttpServlet {
@@ -19,6 +26,7 @@ public class DispatcherServlet extends HttpServlet {
     private static final Logger log = LoggerFactory.getLogger(DispatcherServlet.class);
 
     private RequestMappingHandlerMapping requestMappingHandlerMapping;
+    private List<ViewResolver> viewResolvers;
 
     /**
      * 톰캣이 http Servlet을 싱글톤 객체 하나로 만든다.
@@ -31,6 +39,13 @@ public class DispatcherServlet extends HttpServlet {
         log.info("DispatcherServlet#init");
         requestMappingHandlerMapping = new RequestMappingHandlerMapping();
         requestMappingHandlerMapping.init();
+        /**
+         * [SingletonList]
+         * immutable 즉, 불변성 상수 느낌으로 사용한다.
+         * Arrays.asList는 리스트의 변경이 가능하므로 메모리를 더 사용하게 된다.
+         * 리스트의 요소가 단1개인 불변의 리스트를 생성할 경우에 사용한다.
+         */
+        viewResolvers = Collections.singletonList(new JspViewResolver()); //viewResolver 클래스 싱글톤 리스트로 초기화
     }
 
     @Override
@@ -43,6 +58,14 @@ public class DispatcherServlet extends HttpServlet {
             String viewName = handler.handleRequest(request, response);
 //            RequestDispatcher requestDispatcher = request.getRequestDispatcher(viewName);
 //            requestDispatcher.forward(request, response);
+            /**
+             * ViewResolver의 resolveView메소드를 통해 뷰 네임을 전달하면 적절한 뷰가 선택될 것이고 (Redirect/Forward)
+             * forward 혹은 redirect를 통해 적절한 view를 출력해준다.
+             */
+            for (ViewResolver viewResolver : viewResolvers) {
+                View view = viewResolver.resolveView(viewName);
+                view.render(new HashMap<>(), request, response);
+            }
         } catch (Exception e) {
             log.error("exception occurred: [{}]", e.getMessage(), e);
             throw new ServletException(e);
