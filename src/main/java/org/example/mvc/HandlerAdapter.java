99f112903d3d58b001ef1a2d52776fc6f9996442
webdevyoo <yjk7454@naver.com>
2023-11-15 18:54:38 +0900

    fix: HandlerAdapter 프로덕션 인터페이스 클래스 추상메소드 선언 - supports()/handle()

--- src/main/java/org/example/mvc/HandlerAdapter.java
+++ src/main/java/org/example/mvc/HandlerAdapter.java
@@ -1,4 +1,23 @@
 package org.example.mvc;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public interface HandlerAdapter {
+
+    /**
+     * 전달 받은 handler 객체를 지원하는지 여부 반환
+     * @param handler
+     * @return
+     */
+    boolean supports(Object handler);
+
+    /**
+     * handle 수행 메소드 <br/>
+     * 결과값으로 ModelAndView를 반환받는다.
+     * @param request
+     * @param response
+     * @throws Exception
+     */
+    ModelAndView handle(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
